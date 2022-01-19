d8625d32d2aea1bfbf2751942f817c5e2dd2f96f
rainerprinz <rainer.prinz@uibk.ac.at>
2022-01-19 20:40:14 +0100

    Update solgeo.m

--- solgeo.m
+++ solgeo.m
@@ -22,16 +22,16 @@ delta = (0.006918-0.399912.*cos(gamma)+0.070257.*sin(gamma)-0.006758...
 Et = ( 0.000075 + 0.001868.*cos(gamma) - 0.032077.*sin(gamma)...
      - 0.014615.*cos(2.*gamma) - 0.04089.*sin(2.*gamma) )...
          *229.18; % Equation of time
-LATd = Et + 4*(lon-lsm); % Iqbal(1.4.2) difference to local apparent time
+LATd = Et + 4*(lon-lsm); % Iqbal 1983 (1.4.2) difference to local apparent time
 time_LT = time + minutes(LATd); % local apparent time
-hro = hour(time_LT)+minute(time_LT)/60; % decimal hours of LAT
-omega = (hro-12).*deg2rad(15); % hour angle 0 = 12h LAT
+hro = hour(time_LT)+minute(time_LT)/60; % decimal hours of LAT [h]
+omega = (hro-12).*deg2rad(15); % hour angle 0 = 12h LAT [h]
 latrad = deg2rad(lat);
 sunel = asin(sin(latrad).*sin(delta) + cos(latrad).*cos(delta).*cos(omega)); % sun elevation
-TOA = Io.*Eo.*sin(sunel); % Top of atmosphere radiation
+TOA = Io.*Eo.*sin(sunel); % Top of atmosphere radiation [rad]
 omegas = acos(tan(latrad)*-1.*tan(delta));
 omegas(omegas~=real(omegas)) = NaN;
-LOD = 2/15.*rad2deg(omegas);
+LOD = 2/15.*rad2deg(omegas); % length of day [h]
 
 end
 
